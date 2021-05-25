3918dd548dcd2454974282abf7944704c806f578
Andre Prilly Kurniawan <dreprilly@gmail.com>
2021-05-25 16:01:49 +0700

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart, removeItem, addItem } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="item" />
+const CheckoutItem = ({ cartItem, dispatch }) => { 
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="item" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => dispatch(removeItem(cartItem))}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => dispatch(addItem(cartItem))}>&#10095;</div>
+      </span>
+      <span className="price">{price}</span>
+      <div 
+        className="remove-button"
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+      >&#10005;</div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-button">&#10005;</div>
-  </div>
-);
+  );
+}
 
-export default CheckoutItem;
+export default connect(null)(CheckoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,28 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 }
+
+export const removeItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(
+      cartItem => cartItem.id !== cartItemToRemove.id
+    );
+  }
+
+  return cartItems.map(
+    cartItem => 
+    cartItem.id === cartItemToRemove.id 
+    ? { ...cartItem, quantity: cartItem.quantity - 1}
+    : cartItem
+  )
+}
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  return cartItems.filter(
+    cartItem => cartItem.id !== cartItemToClear.id
+  );
+}
